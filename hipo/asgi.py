cc4bafcf9bf7b764d3d8e4a0ccf1a9383856ab78
Furkan Karataş <furkan.karatas02@gmail.com>
2020-02-10 23:20:18 +0300

    Fix project name

--- hipo/asgi.py
+++ hipo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hipox project.
+ASGI config for hipo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hipox.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hipo.settings")
 
 application = get_asgi_application()
