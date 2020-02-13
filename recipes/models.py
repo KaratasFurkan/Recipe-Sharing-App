b68447c5fe191917581ca5a7e5840015b0d884af
Furkan Karataş <furkan.karatas02@gmail.com>
2020-02-13 22:10:00 +0300

    Fix nullable

--- recipes/models.py
+++ recipes/models.py
@@ -6,7 +6,7 @@ class Recipe(models.Model):
     name = models.CharField(max_length=30)
     image = models.ImageField()
     description = models.TextField(max_length=4000)
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
