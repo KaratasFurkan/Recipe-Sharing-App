90cb1241a5552858a64d9ba22b00dbd8b8bbbffd
Furkan Karataş <furkan.karatas02@gmail.com>
2020-02-13 22:07:54 +0300

    Create models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField()
+    description = models.TextField(max_length=4000)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    EASY = "0"
+    MEDIUM = "1"
+    HARD = "2"
+    DIFFICULTY_CHOICES = [
+        (EASY, "Easy"),
+        (MEDIUM, "Medium"),
+        (HARD, "Hard"),
+    ]
+    difficulty = models.CharField(
+        max_length=1, choices=DIFFICULTY_CHOICES, default=EASY,  # blank=False
+    )
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    recipe = models.ManyToManyField(Recipe)
+
+    # class Meta:
+    #     ordering = ['headline']
+
+
+class Like:
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+
+class Star:
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    rate = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
