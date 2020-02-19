89de9886802116fab464cc43fc0595f7001526d9
Furkan Karataş <furkan.karatas02@gmail.com>
2020-02-19 17:37:56 +0300

    Fix many to many relationship

--- recipes/models.py
+++ recipes/models.py
@@ -6,6 +6,8 @@ class Recipe(models.Model):
     name = models.CharField(max_length=30)
     image = models.ImageField()
     description = models.TextField(max_length=4000)
+    ingredients = models.ManyToManyField("Ingredient")
+
     created_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
@@ -25,7 +27,7 @@ class Recipe(models.Model):
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    recipe = models.ManyToManyField(Recipe)
+    recipes = models.ManyToManyField(Recipe)
 
     # class Meta:
     #     ordering = ['headline']
