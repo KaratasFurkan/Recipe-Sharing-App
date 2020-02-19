356df5c20b4d6572d4b7a4eabf0237efb86abed0
Furkan Karataş <furkan.karatas02@gmail.com>
2020-02-19 18:08:54 +0300

    Create views

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
 
-# Create your views here.
+from .forms import RecipeForm
+from .models import Recipe
+
+
+class RecipeListView(ListView):
+    model = Recipe
+    context_object_name = "recipes"
+    template_name = "home.html"
+
+
+class ShareView(CreateView):
+    model = Recipe
+    form_class = RecipeForm
+    success_url = reverse_lazy("home")
+    template_name = "share.html"
+
+
+def detail(request):
+    return render(request, "detail.html")
