3ab7d17da920f1adc9521ec08af6f071e2a441d4
Furkan Karataş <furkan.karatas02@gmail.com>
2020-02-20 15:31:31 +0300

    Use ListView's fields

--- recipes/views.py
+++ recipes/views.py
@@ -9,16 +9,14 @@ from .models import Ingredient, Recipe
 
 class RecipeListView(ListView):
     model = Recipe
-    queryset = Recipe.objects.order_by("-created_at")
+    ordering = "-created_at"
     context_object_name = "recipes"
-    template_name = "home.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["ingredients"] = Ingredient.objects.annotate(
+    extra_context = {
+        "ingredients": Ingredient.objects.annotate(
             recipe_count=Count("recipe")
         ).order_by("-recipe_count")[:5]
-        return context
+    }
+    template_name = "recipes.html"
 
 
 class ShareView(CreateView):
