bc85a6240fc49f7cb52cbdbe3776a98050d8ffb2
Furkan Karataş <furkan.karatas02@gmail.com>
2020-02-20 13:20:54 +0300

    Sort recipes by date and send ingredients in context too.

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,24 @@
+from django.db.models import Count
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 
 from .forms import RecipeForm
-from .models import Recipe
+from .models import Ingredient, Recipe
 
 
 class RecipeListView(ListView):
     model = Recipe
+    queryset = Recipe.objects.order_by("-created_at")
     context_object_name = "recipes"
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["ingredients"] = Ingredient.objects.annotate(
+            recipe_count=Count("recipe")
+        ).order_by("-recipe_count")[:5]
+        return context
 
 
 class ShareView(CreateView):
