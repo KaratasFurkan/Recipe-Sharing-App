42a4453b0bb89c5f68e81e734f5d25b7d9e3b084
Furkan Karataş <furkan.karatas02@gmail.com>
2020-02-29 16:13:08 +0300

    Login automatically after signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -10,3 +11,8 @@ class Signup(CreateView):
     form_class = SignupForm
     template_name = "signup.html"
     success_url = reverse_lazy("home")
+
+    def form_valid(self, form):
+        valid = super().form_valid(form)
+        login(self.request, self.object)
+        return valid
