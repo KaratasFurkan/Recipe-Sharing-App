f418518b4872fa1cdf2cc8088a36b9a11b44177f
Furkan Karataş <furkan.karatas02@gmail.com>
2020-03-20 01:22:14 +0300

    Add help text to ingredients form

--- recipes/forms.py
+++ recipes/forms.py
@@ -5,10 +5,14 @@ from .models import Recipe
 
 class RecipeForm(forms.ModelForm):
     description = forms.CharField(
-        # TODO: handle in css
+        # NOTE: styling in css probably would be a better practice
         widget=forms.Textarea(attrs={"style": "resize:none;"})
     )
 
     class Meta:
         model = Recipe
         fields = ["name", "image", "description", "difficulty", "ingredients"]
+        help_texts = {
+            "ingredients": "Select Ingredients  (Hold down the Ctrl (windows/linux)"
+            " / Command (Mac) button to select multiple options.)",
+        }
